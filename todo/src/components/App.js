be73dd03799564c6b2d3410169252114d582774f
JaySebagh <jaysebagh@gmail.com>
2019-03-05 15:42:25 -0800

    it works lol. Was kinda rough. Commenting everything was really helpful. Documentation was very helpful.

--- todo/src/components/App.js
+++ todo/src/components/App.js
@@ -2,7 +2,12 @@ import React from 'react';
 import './App.css';
 import AddTodo from '../containers/AddTodo';
 import VisibleTodoList from '../containers/VisibilityTodoList';
-import Footer from './Footer'
+import Footer from './Footer';
+
+// this brings the components together
+// AddTodo is the part where the user can add items to list
+// VisibleTodoList is the actual list displaying
+// Footer is the part where the users can filter the list
 
 const App = () => (
   <div>
