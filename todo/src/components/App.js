d52ec8c1655ee9b48ee08aa99a6d15e091302ac1
JaySebagh <jaysebagh@gmail.com>
2019-03-05 13:03:48 -0800

    Imported files into 'Appjs' and added them to the div. App structure is going to have the Todo input field + button at the top, the actual list in the middle, and the visibility functionality (buttons) at the bottom.

--- todo/src/components/App.js
+++ todo/src/components/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import AddTodo from '../containers/AddTodo';
+import VisibleTodoList from '../containers/VisibilityTodoList';
 
 const App = () => (
   <div>
-    
+    <AddTodo />
+    <VisibleTodoList />
+    <Footer />
   </div>
 )
 
